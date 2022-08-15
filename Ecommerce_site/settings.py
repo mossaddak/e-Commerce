c5b6bc877c19c7308b9a1a01c779c6de09454dc1
mossaddak <10000mossaddak@gmail.com>
2022-08-16 01:38:59 +0600

    third commit

--- Ecommerce_site/settings.py
+++ Ecommerce_site/settings.py
@@ -28,7 +28,7 @@ STATIC_DIR = os.path.join(BASE_DIR,'static')
 SECRET_KEY = 'django-insecure-ibz8y0*04-smjhm=@(rom(jbxegmk9#xtoqsh48v8_w3zom)yz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
