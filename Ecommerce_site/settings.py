3b402463ea03b0e51c9434f64de839466e446047
mossaddak <10000mossaddak@gmail.com>
2022-08-16 01:47:40 +0600

    edit1

--- Ecommerce_site/settings.py
+++ Ecommerce_site/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-ibz8y0*04-smjhm=@(rom(jbxegmk9#xtoqsh48v8_w3zom)yz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ecommercems.herokuapp.com','localhost']
 
 
 # Application definition
